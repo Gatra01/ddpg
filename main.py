b4370093c41aa81fe7fd5aff0f1ce7e6d5b3e340
Gatra01 <87842559+Gatra01@users.noreply.github.com>
2025-02-20 17:46:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,114 @@
-from env import PowerAllocationEnv
-from ddpg import DDPGAgent
 import numpy as np
+import matplotlib.pyplot as plt
+from env import GameState
+from ddpg import *
+from collections import deque
+import torch.nn as nn
+import os, shutil
+import argparse
+from datetime import datetime
+from utils import str2bool,evaluate_policy
 
-def train_ddpg():
-    num_episodes = 500
-    max_steps = 100
-    batch_size = 64
-    state_dim = 10  
-    action_dim = 5  # Power allocation for 5 nodes
-    max_action = 1  # Power max allocation
+'''Hyperparameter Setting'''
+parser = argparse.ArgumentParser()
+parser.add_argument('--dvc', type=str, default='cuda', help='running device: cuda or cpu')
+parser.add_argument('--EnvIdex', type=int, default=0, help='PV1, Lch_Cv2, Humanv4, HCv4, BWv3, BWHv3')
+parser.add_argument('--write', type=str2bool, default=False, help='Use SummaryWriter to record the training')
+parser.add_argument('--render', type=str2bool, default=False, help='Render or Not')
+parser.add_argument('--Loadmodel', type=str2bool, default=False, help='Load pretrained model or Not')
+parser.add_argument('--ModelIdex', type=int, default=100, help='which model to load')
 
-    env = PowerAllocationEnv(num_nodes=5, p_max=1, noise_power=0.01)
-    agent = DDPGAgent(state_dim, action_dim, max_action)
+parser.add_argument('--seed', type=int, default=0, help='random seed')
+parser.add_argument('--Max_train_steps', type=int, default=5e6, help='Max training steps')
+parser.add_argument('--save_interval', type=int, default=1e5, help='Model saving interval, in steps.')
+parser.add_argument('--eval_interval', type=int, default=2e3, help='Model evaluating interval, in steps.')
 
-    for episode in range(num_episodes):
-        state = np.array(env.reset()).flatten()
-        episode_reward = 0
+parser.add_argument('--gamma', type=float, default=0.99, help='Discounted Factor')
+parser.add_argument('--net_width', type=int, default=400, help='Hidden net width, s_dim-400-300-a_dim')
+parser.add_argument('--a_lr', type=float, default=1e-3, help='Learning rate of actor')
+parser.add_argument('--c_lr', type=float, default=1e-3, help='Learning rate of critic')
+parser.add_argument('--batch_size', type=int, default=128, help='batch_size of training')
+parser.add_argument('--random_steps', type=int, default=5e4, help='random steps before trianing')
+parser.add_argument('--noise', type=float, default=0.1, help='exploring noise')
+opt = parser.parse_args()
+opt.dvc = torch.device(opt.dvc) # from str to torch.device
 
-        for step in range(max_steps):
-            action = agent.select_action(state)
-            env.power = action  # Set new power allocation
-            env.hitung_data_rate()  # Update SINR & data rate
-            
-            next_state = np.array(env.observation()).flatten()
-            reward = env.hitung_efisiensi_energi()
-            done = False  # No termination in this environment
-            
-            agent.replay_buffer.add(state, action, reward, next_state, done)
-            agent.train(batch_size)
+def main():
+    #EnvName = ['Pendulum-v1','LunarLanderContinuous-v2','Humanoid-v4','HalfCheetah-v4','BipedalWalker-v3','BipedalWalkerHardcore-v3']
+    #BrifEnvName = ['PV1', 'LLdV2', 'Humanv4', 'HCv4','BWv3', 'BWHv3']
 
-            state = next_state
-            episode_reward += reward
-        
-        print(f"Episode {episode+1}: Reward = {episode_reward:.4f}")
+    # Build Env
+    env = GameState(5,3)
+    eval_env = GameState(5,3)
+    opt.state_dim = env.observation_space
+    opt.action_dim = env.action_space
+    opt.max_action = env.p_max   #remark: action space【-max,max】
+    #print(f'Env:{EnvName[opt.EnvIdex]}  state_dim:{opt.state_dim}  action_dim:{opt.action_dim}  '
+    #      f'max_a:{opt.max_action}  min_a:{env.action_space.low[0]}  max_e_steps:{env._max_episode_steps}')
 
-    print("Training selesai!")
+    # Seed Everything
+    env_seed = opt.seed
+    torch.manual_seed(opt.seed)
+    torch.cuda.manual_seed(opt.seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    print("Random Seed: {}".format(opt.seed))
 
-if __name__ == "__main__":
-    train_ddpg()
+    # Build SummaryWriter to record training curves
+    if opt.write:
+        from torch.utils.tensorboard import SummaryWriter
+        timenow = str(datetime.now())[0:-10]
+        timenow = ' ' + timenow[0:13] + '_' + timenow[-2::]
+        writepath = 'runs/{}'.format(BrifEnvName[opt.EnvIdex]) + timenow
+        if os.path.exists(writepath): shutil.rmtree(writepath)
+        writer = SummaryWriter(log_dir=writepath)
+
+
+    # Build DRL model
+    if not os.path.exists('model'): os.mkdir('model')
+    agent = DDPG_agent(**vars(opt)) # var: transfer argparse to dictionary
+    if opt.Loadmodel: agent.load(BrifEnvName[opt.EnvIdex], opt.ModelIdex)
+
+    if opt.render:
+        while True:
+            score = evaluate_policy(env, agent, turns=1)
+            print('EnvName:', BrifEnvName[opt.EnvIdex], 'score:', score)
+    else:
+        total_steps = 0
+        while total_steps < opt.Max_train_steps:
+            s,info= env.ini(seed=env_seed)  # Do not use opt.seed directly, or it can overfit to opt.seed
+            env_seed += 1
+            done = False
+
+            '''Interact & trian'''
+            while not done:  
+                if total_steps < opt.random_steps: a = env.ini()
+                else: a = agent.select_action(s, deterministic=False)
+                s_next, r, dw, tr, info = env.step(a) # dw: dead&win; tr: truncated
+                done = (dw or tr)
+
+                agent.replay_buffer.add(s, a, r, s_next, dw)
+                s = s_next
+                total_steps += 1
+
+                '''train'''
+                if total_steps >= opt.random_steps:
+                    agent.train()
+
+                '''record & log'''
+                if total_steps % opt.eval_interval == 0:
+                    ep_r = evaluate_policy(eval_env, agent, turns=3)
+                    if opt.write: writer.add_scalar('ep_r', ep_r, global_step=total_steps)
+                    print(f'EnvName:{BrifEnvName[opt.EnvIdex]}, Steps: {int(total_steps/1000)}k, Episode Reward:{ep_r}')
+
+                '''save model'''
+                if total_steps % opt.save_interval == 0:
+                    agent.save(BrifEnvName[opt.EnvIdex], int(total_steps/1000))
+        env.close()
+        eval_env.close()
+
+
+if __name__ == '__main__':
+    main()
+
+
