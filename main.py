c0f016139be0641f71448b43a4c1b230b5fe2943
Gatra01 <87842559+Gatra01@users.noreply.github.com>
2025-02-21 04:25:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ def main():
                 '''record & log'''
                 if total_steps % opt.eval_interval == 0:
                     state_eval=eval_env.ini()
-                    ep_r = evaluate_policy(state_eval, agent, turns=3)
+                    ep_r = evaluate_policy(state_eval,eval_env, agent, turns=3)
                     if opt.write: writer.add_scalar('ep_r', ep_r, global_step=total_steps)
                     print(f'EnvName:{BrifEnvName[opt.EnvIdex]}, Steps: {int(total_steps/1000)}k, Episode Reward:{ep_r}')
 
