87c329c0e836925de2da5fef1eb6b1569caddde1
Gatra01 <87842559+Gatra01@users.noreply.github.com>
2025-02-20 21:38:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ def main():
 
             '''Interact & trian'''
             while not done:  
-                if total_steps < opt.random_steps: a = env.ini()
+                if total_steps < opt.random_steps: a = env.p
                 else: a = agent.select_action(s, deterministic=False)
                 s_next, r, dw, tr, info = env.step(a) # dw: dead&win; tr: truncated
                 done = (dw or tr)
